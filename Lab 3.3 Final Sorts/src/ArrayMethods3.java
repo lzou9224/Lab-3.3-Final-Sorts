6b2940164a128b994af17e0fcddf3d00b0321890
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-13 09:17:47 -0500

    :D

--- Lab 3.3 Final Sorts/src/ArrayMethods3.java
+++ Lab 3.3 Final Sorts/src/ArrayMethods3.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //Lingli Zou
 //2/16/2018
 //Lab 3.23 - Final Sorts
@@ -6,9 +8,16 @@ public class ArrayMethods3 {
 	
 	public static String[] mergeSort(String[] list)
 	{
+		if(list.length==1)
+		{
+			return list;
+		}
+		
+		String[] left= Arrays.copyOfRange(list, 0, list.length/2);
+		String[] right= Arrays.copyOfRange(list, list.length/2, list.length);
 		
 		
-		
+		return(merge(mergeSort(left), mergeSort(right))));
 		//return the sorted string array
 	}
 	
