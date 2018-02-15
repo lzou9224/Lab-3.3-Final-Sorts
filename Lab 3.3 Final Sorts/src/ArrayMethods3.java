ea2f46378717b0e1e7cc8442c257173d1ab83e2b
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-15 09:12:45 -0500

    Almost

--- Lab 3.3 Final Sorts/src/ArrayMethods3.java
+++ Lab 3.3 Final Sorts/src/ArrayMethods3.java
@@ -6,6 +6,30 @@ import java.util.Arrays;
 
 public class ArrayMethods3 {
 	
+	public static void main(String[] args)
+	{
+		//testing arrays
+		String[] test1 = {"apple", "cucumber", "microsoft", "zorro"};
+		String[] test2 = {"banana", "cherry", "mahogany", "oreos", "pinata"};
+		int[] test3 = {3,4,2,7,1};
+		
+		//merge test
+		long start= System.nanoTime();
+		String[] mergeResult= merge(test1, test2);
+		long end= System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took : " + time + " nanoseconds");
+		System.out.println(Arrays.toString(mergeResult));
+		
+		start = System.nanoTime();
+		int pivotFinalPos = partition(test3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition test took : " + time + " nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos); 
+		System.out.println(Arrays.toString(test3));
+		
+	
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		//precondition: both list1 and list2 contains Strings in alphabetical order from a to z 
@@ -63,14 +87,60 @@ public class ArrayMethods3 {
 		return(merge(mergeSort(left), mergeSort(right)));
 	}
 	
-	public static int partition(int[] list1, int front, int back)
+	public static int partition(int[] list, int front, int back)
 	{
 		
+		int temp;
+
 		
+		while(front != back)
+		{
+			if(front < back)
+			{
+				if(list[front] < list[back])
+				{
+					back--;
+				}
+			
+				if(list[back] <= list[front])
+				{
+					temp = list[front];
+					list[front] = list[back];
+					list[back] = temp;
+					
+					temp= back;
+					back= front;
+					front=temp;
+					back++;
+				}
+			}
+			
+			else if(front > back)
+			{
+				if(list[front] > list[back])
+				{
+					back++;
+				}
+			
+				if(list[back] > list[front])
+				{
+					temp = list[back];
+					list[back] = list[front];
+					list[front] = temp;
+					
+					temp=front;
+					front= back;
+					back=temp;
+					back--;
+					
+				}
+			}
 		
+	}		   
 		//return the position of the pivot
+		return front;
+
 	}
-	
 	public static void quickSort(int[] list1, int front, int back)
 	{
 		if(back > front)
